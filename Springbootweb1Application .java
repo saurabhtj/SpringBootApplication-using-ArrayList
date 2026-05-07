5a0ca73c329f029ec05aab736938b579ca317da1
saurabhtj <saurabhjijil@gmail.com>
2026-05-07 22:11:31 +0530

    Create Springbootweb1Application .java

---
+++ Springbootweb1Application .java
@@ -0,0 +1,13 @@
+package springbootweb1.springbootweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootweb1Application 
+{
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(Springbootweb1Application.class, args);
+    }
+}
